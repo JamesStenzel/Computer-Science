524d5a0b0b9f7f9eb406e40b9d3f01ee5c1db05f
JamesStenzel <stenj49@gmail.com>
2024-09-23 09:28:16 -0500

    animal survey

---
+++ Assignments/AnimalSurvey.py
@@ -0,0 +1,6 @@
+input1 = input("What is your favorite animal? ")
+input2 = input("Why do you like them? ")
+input3 = input("Where do they live? ")
+input4 = input("What is a fun fact about them? ")
+input5 = input("What color are they? ")
+print("your favorite animal is the " + input1 + " because " + input2 + "." + " They live in " + input3 + " and " + input4 + ". They are " + input5 + ".")
