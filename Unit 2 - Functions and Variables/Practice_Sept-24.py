ebcb11e367f785c363bbafffe7f11381530e0899
JamesStenzel <stenj49@gmail.com>
2024-09-24 09:40:46 -0500

    practice

---
+++ Unit 2 - Functions and Variables/Practice_Sept-24.py
@@ -0,0 +1,7 @@
+def number():
+    my_number = 4           #Declaration - Creating
+    my_number = 5           #Assignment - Changing
+    print(my_number)
+
+input_1 = int(input("enter a number\n>"))
+print(input_1 * 10)
