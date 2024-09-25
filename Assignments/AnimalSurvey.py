6da69066cfca0d4ae8198e0c91670bb9f776327b
JamesStenzel <stenj49@gmail.com>
2024-09-25 09:28:24 -0500

    Comments Added

--- Assignments/AnimalSurvey.py
+++ Assignments/AnimalSurvey.py
@@ -1,6 +1,6 @@
-input1 = input("What is your favorite animal? ")
+input1 = input("What is your favorite animal? ") #lines 1-4 take input data from the user
 input2 = input("Why do you like them? ")
 input3 = input("Where do they live? ")
 input4 = input("What is a fun fact about them? ")
 input5 = input("What color are they? ")
-print("your favorite animal is the " + input1 + " because " + input2 + "." + " They live in " + input3 + " and " + input4 + ". They are " + input5 + ".")
+print("your favorite animal is the " + input1 + " because " + input2 + "." + " They live in " + input3 + " and " + input4 + ". They are " + input5 + ".") #prints summary of data taken from user
