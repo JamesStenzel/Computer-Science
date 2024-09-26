4c51b667582c75c3e5b9344901a6f6539795874e
JamesStenzel <stenj49@gmail.com>
2024-09-26 09:25:39 -0500

    mad lib

---
+++ Assignments/SimpleMadLib.py
@@ -0,0 +1,18 @@
+#once apon a time a(n) [adjective] [noun] was livng in a [noun].
+#one day, the [1st adjective and noun] went to the store hoping to get a(n) [adjective] [noun].
+#but, when he got there the [previous adjectiv and noun] was gone!
+#so instead, the [first asjective and noun] went to the [location].
+#after he got there a(n) [noun] had stole his [noun]!
+#so, after all that the [first adjective and noun] just went home
+
+print("Welcome to the my mad lib. Complete it by filling in the blanks.")
+char_adj = input("adjective\n>")
+char_noun = input("noun\n>")
+loc_noun = input("noun\n>")
+thing_adj = input("adjective\n>")
+thing_noun = input("noun\n>")
+location = input("location\n>")
+bad_noun = input("noun\n>")
+keys_noun = input("noun\n>")
+
+print("Once apon a time, a(n) ",char_adj," ",char_noun,"was living in a(n) ",loc_noun,".\n","one day, the ",char_adj," ", char_noun," went to the store hoping to get a(n)",thing_adj," ",thing_noun,".\n>","But, when he got there the",thing_adj," ",thing_noun," was gone!\n","so, instead, the ",char_adj," ",char_noun," went to the ",location,".\n","after he got there a(n) ",bad_noun," had stole his ",keys_noun,"!\n","So, after all that, the ", char_adj," ",char_noun," just went home.")
