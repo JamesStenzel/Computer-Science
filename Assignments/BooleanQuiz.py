31933451313a5ebfe461eafb6638a776e0603d19
JamesStenzel <stenj49@gmail.com>
2024-10-04 09:09:36 -0500

    Boolean Quiz

---
+++ Assignments/BooleanQuiz.py
@@ -0,0 +1,14 @@
+input_1 = int(input("provide a number greater than or equal to 4\n>"))
+print(input_1 >= 4)
+
+input_2 = int(input("provide a number less than 255\n>"))
+print(input_2 < 255)
+
+input_3 = int(input("provide a number greater then 10,857\n>"))
+print(input_3 > 10857)
+
+input_4 = int(input("provide a number less than or equal to 6\n>"))
+print(input_4 <= 6)
+
+input_5 = int(input("provide a number equal to 15,028,748,325\n>"))
+print(input_5 == 15028748325)
