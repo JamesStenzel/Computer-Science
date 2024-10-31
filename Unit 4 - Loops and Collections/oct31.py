1e0cf2e115f84fabd29d073ae2ff49956619d8b3
JamesStenzel <stenj49@gmail.com>
2024-10-31 09:34:37 -0500

    practice

---
+++ Unit 4 - Loops and Collections/oct31.py
@@ -0,0 +1,21 @@
+import random
+best_elden_ring_weapons = ["Blasphemous blade", "Moonveil", "Rivers of Blood", "Iron Ball", "Bloodhound's fang"]
+
+#Index        0     1     2     3
+best_years = [1776, 1985, 1994, 1957]
+print(best_elden_ring_weapons[int(len(best_elden_ring_weapons))-1])
+
+#Print worst best elden ring weapon
+print(len(best_elden_ring_weapons))
+
+#Strings are lists of characters
+word = "List"
+a = random.randint(0,3)
+print(word[a])
+
+#removes item at the position in the list
+best_elden_ring_weapons.pop(4)
+
+#remove item by value
+#removes first occurrance of an item
+best_elden_ring_weapons.remove("Moonveil")
